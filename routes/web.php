2ae4f698fee345244b10c86430f40c64d5b240ae
shane <shane.kokoperi@gmail.com>
2018-11-25 03:44:59 +0900

    ユーザー認証の実装

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::get('/tweets', 'TweetController@index');//tweetsというURLにアクセスされたらTweetControllerのindexメソッドを実行する。indexでは一覧表示
 Route::get('/tweets/create', 'TweetController@create');//新規投稿画面の表示
 Route::post('/tweets', 'TweetController@store');//新規投稿されたときにpostで受け取る
@@ -21,3 +17,7 @@ Route::get('/tweets/{id}', 'TweetController@show');//各ツイートにある詳
 Route::get('/tweets/{id}/edit', 'TweetController@edit');//ツイート詳細の更新が押されたときにそのツイートのidを持ったままeditメソッドを呼ぶ
 Route::put('/tweets/{id}', 'TweetController@update');//ツイートの更新が押されたときにのツイートのidを持ってupdateメソッドを呼ぶ、なんかよくわからんけどputを使う
 Route::delete('/tweets/{id}', 'TweetController@destroy');//ツイートの削除が押されたときにのツイートのidを持ってdestroyメソッドを呼ぶ、なんかよくわからんけどdeleteを使う
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
